f36623383d396a3751380ceaf80b18aad0a5728f
Gautier Nilhcem <nilhcem@gmail.com>
2014-12-06 13:46:26 +0100

    Save screenshot in external storage

--- app/src/main/java/com/nilhcem/snapchat/xoxo/CountdownService.java
+++ app/src/main/java/com/nilhcem/snapchat/xoxo/CountdownService.java
@@ -4,15 +4,25 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v4.app.NotificationCompat;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Locale;
 
 public class CountdownService extends IntentService {
 
-    private static int FOREGROUND_ID = 1338;
+    private static final int FOREGROUND_ID = 1338;
     private static final long SLEEP_TIME = 4000;
+
+    private static final String OUTPUT_DIR = "snapchatxoxo";
+    private static final String CHARSET_NAME = "ASCII";
+    private static final String SCREENSHOTS_EXT = ".png";
+
+    private static final String CMD_SU = "su";
+    private static final String CMD_SCREENCAP = "/system/bin/screencap -p %s";
 
     public CountdownService() {
         super(CountdownService.class.getSimpleName());
@@ -45,13 +55,22 @@ public class CountdownService extends IntentService {
 
     private void takeScreenshotAsRoot() {
         Process sh;
+        File screenshot = new File(getImagesDirectory(), Long.toString(System.currentTimeMillis()) + SCREENSHOTS_EXT);
+
         try {
-            sh = Runtime.getRuntime().exec("su", null, null);
+            sh = Runtime.getRuntime().exec(CMD_SU, null, null);
             OutputStream os = sh.getOutputStream();
-            os.write(("/system/bin/screencap -p " + "/sdcard/snapshat-" + Long.toString(System.currentTimeMillis()) + ".png").getBytes("ASCII"));
+            os.write((String.format(Locale.US, CMD_SCREENCAP, screenshot.getAbsolutePath())).getBytes(CHARSET_NAME));
             os.flush();
             os.close();
         } catch (IOException e) {
         }
+    }
+
+    private File getImagesDirectory() {
+        File extStore = Environment.getExternalStorageDirectory();
+        File outputDir = new File(extStore, OUTPUT_DIR);
+        outputDir.mkdirs();
+        return outputDir;
     }
 }
